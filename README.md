aeb90b7ff284f4ad9cc2896f06d8b5a6e3e861d2
Vatsal Veerwal <vatsalveerwal@gmail.com>
2024-08-04 18:53:51 +0530

    Improved for all semesters.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # SGPA-analysis
+
 ## Get Started
-- Since GitHub doesn't allow uploading files with size more than 25 mb, first [download](https://drive.google.com/drive/folders/1q6nU_XhFzK8HdcghuysK8_s0zN3lk4PY?usp=sharing) the PDF files from the IMS portal.
-- Rename the files as `first sem.pdf`, `third sem.pdf`, `fifth sem.pdf`, `seventh sem.pdf`.
-- Place the files in the same folder as `create_data.py`.
+
+- Since GitHub doesn't allow uploading files with size more than 25 mb, first download the PDF files from the IMS portal. [(odd semesters)](https://drive.google.com/drive/folders/1q6nU_XhFzK8HdcghuysK8_s0zN3lk4PY?usp=sharing) [(even semesters)](https://drive.google.com/drive/folders/1N124oe3kIu0wC4QF9FkECCfvz3QVGgA6)
+- Rename the files as `1.pdf`, `2.pdf`, `3.pdf`, and so on for each semester.
+- Place the files in the folder `data/pdf`.
 - Now run `create_data.py`. This should create csv files with the roll numbers and SGPA of students. ☢️ **Note that sometimes the roll number is missing for some reason. So far, I have encountered only 2 instances and those too only in the third sem file. For now, you will have to update them manually.**
 - Now that the data has been created, it is time to analyse. Open the `analysis.ipynb` file and run the code blocks in it.
--- create_data.py
+++ create_data.py
@@ -1,12 +1,13 @@
 import PyPDF2
 import re
 import csv
+import os
 
-sems = ["first sem", "third sem", "fifth sem", "seventh sem"]
+sems = [i for i in range(1, 9)]
 
 def create_csv(file_name):
-    pdf_file = open(f"{file_name}.pdf", "rb")
-    csv_file = open(f"{file_name}.csv", "w")
+    pdf_file = open(f"data/pdf/{file_name}.pdf", "rb")
+    csv_file = open(f"data/csv/{file_name}.csv", "w+")
     writer = csv.writer(csv_file)
     writer.writerow(["Roll number", "SGPA"])
     pdf = PyPDF2.PdfReader(pdf_file)
@@ -16,12 +17,12 @@ def create_csv(file_name):
         text = page.extract_text()
         row = []
         for i in range(len(text)):
-            if i + 11 < len(text) and regex.match(text[i:i+11]):
-                row.append(text[i:i+11])
-            if text[i:i+2] == "RL":
+            if i + 11 < len(text) and regex.match(text[i : i + 11]):
+                row.append(text[i : i + 11])
+            if text[i : i + 2] == "RL":
                 row = []
             if text[i] == "." and text[i - 1].isdigit():
-                row.append(text[i-1:i+3])
+                row.append(text[i - 1 : i + 3])
                 writer.writerow(row)
                 row = []
 
@@ -29,4 +30,5 @@ def create_csv(file_name):
     csv_file.close()
 
 for sem in sems:
+    os.makedirs("data/csv", exist_ok=True)
     create_csv(sem)
